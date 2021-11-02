9bb21d5c23d7efcd6a4df3ebf41365fb9a66eb5f
Nacho López Sánchez <ignaciolopezsanchez@MacBook-Pro-de-Nacho-2.local>
2021-11-02 20:36:19 +0100

    key

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ function authenticateToken(req, res, next) {
 
   if (token == null) return res.sendStatus(401);
 
-  jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+  jwt.verify(token, config.key, (err, user) => {
     console.log(err);
 
     if (err) return res.sendStatus(403);
