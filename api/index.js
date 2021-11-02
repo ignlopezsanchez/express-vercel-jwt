f69ef4b237d218aaed9e61e054c0b6090069b436
Nacho López Sánchez <ignaciolopezsanchez@MacBook-Pro-de-Nacho-2.local>
2021-11-02 19:51:06 +0100

    34

--- api/index.js
+++ api/index.js
@@ -7,7 +7,6 @@ app.set("key", config.key);
 app.use(express.urlencoded());
 app.use(express.json());
 app.get("/api", (req, res) => {
-  const path = `/api/item/${v4()}`;
   res.setHeader("Content-Type", "text/html");
   res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
   res.json({ hola: "pepe" });
