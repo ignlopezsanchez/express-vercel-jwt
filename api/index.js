be88f93c45369fb61fa83f9f5221ea809a2fa23b
Nacho López Sánchez <ignaciolopezsanchez@MacBook-Pro-de-Nacho-2.local>
2021-11-02 19:56:46 +0100

    fdf

--- api/index.js
+++ api/index.js
@@ -5,9 +5,8 @@ const app = express();
 app.set("key", config.key);
 app.use(express.urlencoded());
 app.use(express.json());
+
 app.get("/api", (req, res) => {
-  res.setHeader("Content-Type", "text/html");
-  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
   res.json({ hola: "pepe" });
 });
 
@@ -30,10 +29,7 @@ protectedRoutes.use((req, res, next) => {
     });
   }
 });
-app.get("/api/item/:slug", (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
-});
+
 app.post("/api/login", (req, res) => {
   if (req.body.user === "mock_user" && req.body.password === "mock_password") {
     const payload = {
